b5d4bf2403a906483bb582b806c47a87d281bb92
dhea fesa <dhea.fesaathallah@gmail.com>
2021-06-21 21:52:31 +0700

    Cache JSON and Image from API

--- src/App.js
+++ src/App.js
@@ -1,22 +1,40 @@
-import React from 'react';
-import Header from './component/Header';
-import Hero from './component/Hero';
-import Browse from './component/Browse';
-import Arrived from './component/Arrived';
-import Clients from './component/Clients';
-import ASideMenu from './component/ASideMenu';
-import Footer from './component/Footer';
+import React, { useState, useEffect } from "react";
+import Header from "./component/Header";
+import Hero from "./component/Hero";
+import Browse from "./component/Browse";
+import Arrived from "./component/Arrived";
+import Clients from "./component/Clients";
+import ASideMenu from "./component/ASideMenu";
+import Footer from "./component/Footer";
 
 function App() {
+  const [items, setItems] = useState([]);
+
+  useEffect(function () {
+    (async function () {
+      const response = await fetch(
+        'https://prod-qore-app.qorebase.io/8ySrll0jkMkSJVk/allItems/rows?limit=7&offset=0&$order=asc',
+        {
+          headers: {
+            "Content-Type": "application/json",
+            "accept": "application/json",
+            "x-api-key": process.env.REACT_APP_APIKEY,
+          },
+        }
+      );
+      const { nodes } = await response.json();
+      setItems(nodes);
+    })();
+  }, []);
   return (
     <>
-    <Header />
-    <Hero />
-    <Browse />
-    <Arrived />
-    <Clients />
-    <ASideMenu />
-    <Footer />
+      <Header />
+      <Hero />
+      <Browse />
+      <Arrived items={items} />
+      <Clients />
+      <ASideMenu />
+      <Footer />
     </>
   );
 }
